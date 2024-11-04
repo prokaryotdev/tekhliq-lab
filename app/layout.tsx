8f545e8cf11149f7eba63764d7e9910c901e759d
prokaryotdev <cosmiceclipse69@gmail.com>
2024-11-04 09:59:15 -0800

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,20 +8,19 @@ import "./globals.css";
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Harness AI with Custom LLM Applications | Tekhliq Labs",
+  title: "Tekhliq Labs",
   description:
     "Tekhliq Labs helps organizations harness the power of artificial intelligence through custom LLM applications and AI agent workflows. Discover tailored solutions for your business.",
   keywords:
     "artificial intelligence, AI applications, custom LLM, AI workflows, Tekhliq Labs",
   openGraph: {
     title: "Harness AI with Custom LLM Applications | Tekhliq Labs",
-    description:
-      "Unlock the potential of AI with Tekhliq Labs. We specialize in custom LLM applications and AI agent workflows tailored to your organization's needs.",
+    description: "Unlock the potential of AI with Tekhliq Labs.",
     url: "https://tekhliq-lab.vercel.app/",
     siteName: "Tekhliq Labs",
     images: [
       {
-        url: "/open-graph.png",
+        url: "/og-image.png",
         alt: "Tekhliq Labs AI Solutions",
       },
     ],
